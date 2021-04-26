9adfa30f8df1a3aa582840ec8b1d5aae14e8f5c1
Gregor Lohaus <lohausgregor@gmail.com>
2021-04-26 04:54:51 +0200

    added step functionality

--- src/Resources/app/storefront/src/script/QuantitySelect.js
+++ src/Resources/app/storefront/src/script/QuantitySelect.js
@@ -5,31 +5,36 @@ export default class QuantitySelect extends Plugin {
         console.log('Registered')
         this.minus = DomAccess.querySelector(this.el, '.decrease');
         this.plus = DomAccess.querySelector(this.el, '.increase');
-        this.field = DomAccess.querySelector(this.el, '.my-form-control')
-        console.log(this.field.max)
-        console.log(this.field.min)
-        this.registerEvents()
+        this.field = DomAccess.querySelector(this.el, '.my-form-control');
+        this.purchaseStepsElem = DomAccess.querySelector(this.el, '.purchaseSteps');
+        this.step = parseInt(this.purchaseStepsElem.value);
+        this.min = parseInt(this.field.value);
+        this.max = parseInt(this.field.max);
+        //console.log(this.purchaseSteps);
+        //console.log(this.field.max);
+        console.log(this.field.min);
+        this.registerEvents();
     }
 
     registerEvents() {
-        this.minus.addEventListener('click', this.decrease.bind(this))
-        this.plus.addEventListener('click', this.increase.bind(this))
+        this.minus.addEventListener('click', this.decrease.bind(this));
+        this.plus.addEventListener('click', this.increase.bind(this));
 
     }
 
     decrease() {
-        console.log('min'+parseInt(this.field.min)+'     value:'+ parseInt(this.field.value))
-        if (this.field.value > this.field.min) {
-            this.field.value -= 1;
+        //console.log('min'+parseInt(this.field.min)+'     value:'+ parseInt(this.field.value))
+        if (parseInt(this.field.value) - this.step >= this.min ) {
+            this.field.value = parseInt(this.field.value) -this.step ;
         }
 
     }
 
     increase() {
-        console.log('max:'+this.field.max+'     value:'+ this.field.value)
-        if (parseInt(this.field.value) < parseInt(this.field.max)) {
-            console.log('increase if true')
-            this.field.value = parseInt(this.field.value) + 1;
+        //console.log('max:'+this.field.max+'     value:'+ this.field.value)
+        if (parseInt(this.field.value) + this.step <= this.max ) {
+            console.log('increase if true');
+            this.field.value = parseInt(this.field.value) + this.step;
         }
     }
 }
